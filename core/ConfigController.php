60830f414678c79c757852010135efbbef54dd18
Renato Lopes <renato.lop.jr@gmail.com>
2023-02-17 21:04:46 -0300

    Adicionando arquivos essenciais

---
+++ core/ConfigController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Core;
+
+class ConfigController
+{
+    private string $url;
+    private array $urlArray;    
+    private string $urlController;
+    private string $urlParameter;
+    private string $urlSlugController;
+    private array $format;  
+
+    public function __construct()
+    {
+        if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
+            $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
+
+            $this->clearUrl();
+
+            $this->urlArray = explode("/", $this->url);
+
+            if(isset($this->urlArray[0])){
+                $this->urlController = $this->slugController($this->urlArray[0]);
+            }else{
+                $this->urlController = "Home";
+            }
+        }else{
+
+        }        
+    }
+
+    private function clearUrl()
+    {
+        //Eliminar as tag
+        $this->url = strip_tags($this->url);
+   
+        //Aliminar a barra no final da Url
+        $this->url = rtrim($this->url, "/");
+        //Eliminar os espaços em branco
+        $this->url = trim($this->url);
+  
+    }
+
+    private function slugController($slugController)
+    {
+        $this->urlSlugController = strtolower($slugController);
+        $this->urlSlugController = str_replace("-", " ",  $this->urlSlugController);
+        $this->urlSlugController = ucwords($this->urlSlugController);
+        $this->urlSlugController = str_replace(" ", "",  $this->urlSlugController);
+
+        return $this->urlSlugController;
+    }
+
+    public function loadPage()
+    {
+         $classLoad = "\\Sts\\Controllers\\" . $this->urlController;
+         $classPage = new $classLoad();
+         $classPage->index();
+        
+    }
+
+    
+}
